800070bed8353454977edcd90f92f66027c5060e
Ezequiel Baptista <ebaptista@trackingpremium.com>
2026-01-13 16:42:33 -0400

    Add CORS support for API requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+const cors = require('cors');
 
 const api = axios.create({
     baseURL: 'https://backend-cizt-lax5.onrender.com/api'
@@ -9,5 +10,8 @@ const token = localStorage.getItem('token');
 if(token){
     api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
 }
+
+const allowedOrigins = ['https://frontend2-nine-rho.vercel.app'];
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 export default api;
