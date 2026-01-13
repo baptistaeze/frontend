c9737eae190de5771fe08329bb5009914917ee6e
Ezequiel Baptista <ebaptista@trackingpremium.com>
2026-01-13 17:30:38 -0400

    Remove CORS middleware and related code from API service

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
 import axios from 'axios';
-const cors = require('cors');
 
 const api = axios.create({
     baseURL: 'https://backend-cizt-lax5.onrender.com/api'
@@ -10,8 +9,5 @@ const token = localStorage.getItem('token');
 if(token){
     api.defaults.headers.common['Authorization'] = 'Bearer ' + token;
 }
-
-const allowedOrigins = ['https://frontend2-nine-rho.vercel.app'];
-app.use(cors({ origin: allowedOrigins, credentials: true }));
 
 export default api;
